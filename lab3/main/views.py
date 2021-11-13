83b4e33f783b0c8e6a78ba9502cb78f415af5755
oleksandra-yanovych <alya.is.weeping@gmail.com>
2021-11-13 21:27:19 +0200

    Lab4: my_work.log

--- lab3/main/views.py
+++ lab3/main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import os
+from datetime import datetime
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'main.html', {'parameter': "test"})
+
+
+def health(request):
+    osInfo = os.uname()
+    response = {
+    'date': datetime.now().strftime("%d.%m.%y %H:%M"),
+    'current_page': request.get_host() + request.get_full_path(),
+    'server_info': f"OSName: {osInfo.sysname}; NodeName: {osInfo.nodename}; Release:{osInfo.release}; Version:{osInfo.version}; Indentificator:{osInfo.machine}",
+    'client_info': f"Browser: {request.META['HTTP_USER_AGENT']}  IP: {request.META['REMOTE_ADDR']} "
+    }
+    return JsonResponse(response)
